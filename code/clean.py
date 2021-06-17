90b35340eb233a1846fa3438658fb137c444ed55
andrew-gerlach <arg151@pitt.edu>
2021-06-17 17:14:34 -0400

    First attempt failure (no surprise), adding test file for figuring out what's up

--- code/clean.py
+++ code/clean.py
@@ -1,4 +1,6 @@
 import numpy as np
+import json
+import matplotlib.pyplot as mpl
 
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     # fs, lowcut and highcut are in frequency units (Hz)
@@ -20,4 +22,25 @@ def gaussian_lowpass_filter(data, fs, fwhm):
     sigma = fs*fwhm #
     return gaussian_filter1d(data, sigma)
 
-def filter_signals(json, signal)
+################################################################################
+# main routine to read in signals and apply appropriate filter                 #
+# in:  meta - json file containing frequencies for filtering                   #
+#      sigFile - npy file containing processed signals                         #
+################################################################################
+
+#def filter_signals(meta, sigFile):
+
+sigFile = 'signal.npy'
+meta = 'meta.txt'
+
+signal = np.load(sigFile)
+meta = json.load(open(meta))
+cardRange = meta['frequencyRanges'][0]['Cardiac']
+respRange = meta['frequencyRanges'][0]['Respiratory']
+
+# TODO: verify the 0.001 is right and signal is being filtered properly!
+PULS = butter_bandpass_filter(signal[:, -2], cardRange[0], cardRange[1], 0.001)
+
+mpl.plot(signal[:,-2], 'r')
+mpl.plot(PULS, 'b')
+mpl.show()
