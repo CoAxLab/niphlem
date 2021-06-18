2edcb3a931a91d2cfbab61e7b09ba5e0e7e21b5e
andrew-gerlach <arg151@pitt.edu>
2021-06-17 21:01:16 -0400

    PULS looks reasonable now, RESP not so much, ECG seems basically unaffected by filtering (probably doing this one wrong)

--- code/clean.py
+++ code/clean.py
@@ -34,13 +34,32 @@ sigFile = 'signal.npy'
 meta = 'meta.txt'
 
 signal = np.load(sigFile)
+nch = signal.shape[1]-3
+
 meta = json.load(open(meta))
 cardRange = meta['frequencyRanges'][0]['Cardiac']
 respRange = meta['frequencyRanges'][0]['Respiratory']
 
-# TODO: verify the 0.001 is right and signal is being filtered properly!
-PULS = butter_bandpass_filter(signal[:, -2], cardRange[0], cardRange[1], 0.001)
+sf = 1000
+# TODO: verify 1000 Hz is right
+#       extract this from the data
+#       make sure the filter is working as expected
+#       try to rectify the shift
+PULS = butter_bandpass_filter(signal[:, -2], 0.5*cardRange[0], 2*cardRange[1], sf)
+RESP = butter_bandpass_filter(signal[:, -1], 0.5*respRange[0], 2*respRange[1], sf)
+ECG = np.zeros((len(signal), nch))
+for i in range(nch):
+  ECG[:, i] = signal[:, i]-gaussian_lowpass_filter(signal[:, i], sf, 0.5/cardRange[1])
 
-mpl.plot(signal[:,-2], 'r')
+mpl.plot(signal[:,-2]-signal[:,-2].mean(), 'r')
 mpl.plot(PULS, 'b')
 mpl.show()
+
+mpl.plot(signal[:,-1]-signal[:,-1].mean(), 'r')
+mpl.plot(PULS, 'b')
+mpl.show()
+
+mpl.plot(signal[:,1]-signal[:,1].mean(), 'r')
+mpl.plot(ECG[:,1], 'b')
+mpl.show()
+
