fca343a378674e9f12036fabe1d71d739f435efc
jrasero <jrasero.daparte@gmail.com>
2021-09-16 12:38:47 -0400

    add zcore function import

--- code/regressors.py
+++ code/regressors.py
@@ -8,7 +8,7 @@ from sklearn.base import BaseEstimator
 from sklearn.utils.validation import (check_array, column_or_1d)
 
 from events import compute_max_events
-from clean import _transform_filter
+from clean import _transform_filter, zscore
 
 
 class BasePhysio(BaseEstimator):
