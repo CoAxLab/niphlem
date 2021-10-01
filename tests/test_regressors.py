4393788b94db82f10d95f2e120871a2f439d5d37
jrasero <jrasero.daparte@gmail.com>
2021-10-01 12:49:11 -0400

    update notebook with analyses and clean a bit test regressors file

--- tests/test_regressors.py
+++ tests/test_regressors.py
@@ -9,9 +9,9 @@ from regressors import BasePhysio
 
 
 class TestPhysio(BasePhysio):
-    "This is just the Base class that does not compute any regressors."
+    "This is just the Base class that here does not compute any regressors."
     " It is only for testing previous transformations and the several"
-    " checkings ond the input parameters"
+    " checkings ond the input parameters."
 
     def _process_regressors(self,
                             signal,
@@ -47,7 +47,7 @@ def test_initial_checks():
     print(f"we have passed the test: {exc_info.value.args[0]}")
 
     # This should give an error because we are not passing the right
-    # number of columns
+    # number of columns.
     test_physio = TestPhysio(physio_rate=400, t_r=2.0, columns=[1, 2])
     with pytest.raises(ValueError) as exc_info:
         test_physio.compute_regressors(signal,
@@ -73,7 +73,7 @@ def test_columns_param():
     signal13 -= signal13.mean()
 
     # add a fake time scan. It doesn't matter. It will not be used, but
-    # we have to pass one with the same observations of the signal
+    # we have to pass one with the same number of observations of the signal.
     time_scan = rng.randn(100)
 
     test_mean = TestPhysio(physio_rate=400, t_r=2.0, columns="mean")
@@ -84,7 +84,7 @@ def test_columns_param():
     test_list_mean = TestPhysio(physio_rate=400, t_r=2.0,
                                 columns=[1/3, 1/3, 1/3])
     # add a fake time scan. It doesn't matter. It will not be used, but
-    # we have to pass one with the same observations of the signal
+    # we have to pass one with the same number of observations of the signal.
     assert np.allclose(
         test_list_mean.compute_regressors(signal, time_scan)[:, 0],
         mean_signal
